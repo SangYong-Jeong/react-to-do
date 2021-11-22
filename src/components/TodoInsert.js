51b2192a740cf38a9555603493b1a9edc4bb2e3f
SangYong-Jeong <mmaf@naver.com>
2021-11-22 21:22:09 +0900

    todo 앱 완료!

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+      // 이를 방지하기 위해 이 함수를 호출합니다.
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
@@ -14,3 +35,6 @@ const TodoInsert = () => {
 };
 
 export default TodoInsert;
+
+// ** onSubmit 이벤트는 인풋에서 Enter를 눌렀을 때도 발생한다.
+// *** but onClick 이벤트는 클릭했을 때만 이벤트가 발생한다.
